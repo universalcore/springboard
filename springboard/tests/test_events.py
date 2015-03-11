dd5c4318a83c7bdab8a4ac4f0138d3da22eef191
Simon de Haan <simon@praekeltfoundation.org>
2015-03-11 19:28:00 +0200

    test for ga_context decorator

--- springboard/tests/test_events.py
+++ springboard/tests/test_events.py
@@ -12,15 +12,17 @@ class TestEvents(SpringboardTestCase):
         self.app = self.mk_app(self.workspace, settings={
             'ga.profile_id': 'UA-some-id',
         })
+        [self.category] = self.mk_categories(self.workspace, count=1)
+        [self.page] = self.mk_pages(self.workspace, count=1,
+                                    primary_category=self.category.uuid,
+                                    created_at=datetime.now().isoformat())
+
+        self.workspace.save(self.category, 'Add category')
+        self.workspace.save(self.page, 'Add page')
+        self.workspace.refresh_index()
 
     @mock.patch('unicore.google.tasks.pageview.delay')
     def test_ga_pageviews(self, mock_task):
-        [category] = self.mk_categories(self.workspace, count=1)
-        [page] = self.mk_pages(self.workspace, count=1,
-                               primary_category=category.uuid,
-                               created_at=datetime.now().isoformat())
-        self.workspace.save(page, 'Add page')
-        self.workspace.refresh_index()
 
         self.app.get('/', status=200, extra_environ={
             'HTTP_HOST': 'some.site.com',
@@ -34,7 +36,7 @@ class TestEvents(SpringboardTestCase):
         self.assertEqual(data['dh'], 'some.site.com')
         self.assertEqual(data['dr'], '')
 
-        page_url = '/page/%s/' % page.uuid
+        page_url = '/page/%s/' % (self.page.uuid,)
         headers = {
             'User-agent': 'Mozilla/5.0',
         }
@@ -51,6 +53,30 @@ class TestEvents(SpringboardTestCase):
         self.assertEqual(data['uip'], '192.0.0.1')
         self.assertEqual(data['dh'], 'some.site.com')
         self.assertEqual(data['user_agent'], 'Mozilla/5.0')
+        self.assertEqual(data['dt'], self.page.title)
 
         # # ensure cid is the same across calls
         self.assertEqual(gen_client_id, client_id)
+
+    @mock.patch('unicore.google.tasks.pageview.delay')
+    def test_ga_context_decorator(self, mock_task):
+        page_url = '/page/%s/' % (self.page.uuid,)
+        category_url = '/category/%s/' % (self.category.uuid,)
+        headers = {
+            'User-agent': 'Mozilla/5.0',
+        }
+        self.app.get(category_url, status=200, headers=headers, extra_environ={
+            'HTTP_REFERER': '/',
+            'HTTP_HOST': 'some.site.com',
+            'REMOTE_ADDR': '192.0.0.1',
+        })
+        ((profile_id, client_id, data), _) = mock_task.call_args_list[0]
+        self.assertEqual(data['dt'], self.category.title)
+
+        self.app.get(page_url, status=200, headers=headers, extra_environ={
+            'HTTP_REFERER': category_url,
+            'HTTP_HOST': 'some.site.com',
+            'REMOTE_ADDR': '192.0.0.1',
+        })
+        ((profile_id, client_id, data), _) = mock_task.call_args_list[1]
+        self.assertEqual(data['dt'], self.page.title)
