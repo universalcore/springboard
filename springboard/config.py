55e2411b6c73efec0625632331899d15dc22599a
Rizmari Versfeld <rizziepit@gmail.com>
2015-03-18 10:31:17 +0200

    add SSO flow views and put user data on request

--- springboard/config.py
+++ springboard/config.py
@@ -1,8 +1,10 @@
 def includeme(config):
+    config.include('springboard.auth')
     config.add_static_view('static', 'springboard:static', cache_max_age=3600)
     config.add_route('home', '/')
     config.add_route('category', '/category/{uuid}/')
     config.add_route('page', '/page/{uuid}/')
+    # NB: /{slug}/ hides other /[^/]+/ routes - keep it last
     config.add_route('flat_page', '/{slug}/')
     config.add_route('api_notify', '/api/notify/', request_method='POST')
     config.scan(".views")
