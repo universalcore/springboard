1885e06f9b497fde37fe285e11164504e221ade7
nathanbegbie <nathanbegbie@gmail.com>
2015-05-04 12:52:56 +0200

    Added locale selection

--- springboard/views.py
+++ springboard/views.py
@@ -1,9 +1,16 @@
 import os
 
+from babel import Locale
+from pycountry import languages
+
+from ast import literal_eval
+
 from elasticgit import EG
 
 from pyramid.view import view_config
 from pyramid.view import notfound_view_config
+from pyramid.httpexceptions import HTTPFound
+from pyramid.response import Response
 
 from springboard.utils import parse_repo_name, ga_context
 
@@ -80,3 +87,63 @@ class SpringboardViews(object):
     def notfound(self):
         self.request.response.status = 404
         return {}
+
+    @view_config(
+        route_name='locale_change',
+        renderer='cms:templates/locale_change.jinja2')
+    def locale_change(self):
+        return {
+            'languages': self.get_featured_languages() +
+            sorted(list(set(self.get_available_languages) -
+                        set(self.get_featured_languages())),
+                   key=lambda tup: tup[1].lower())
+        }
+
+    @view_config(route_name='locale')
+    @view_config(route_name='locale_matched')
+    def set_locale_cookie(self):
+        response = Response()
+        language = self.request.matchdict.get('language') or \
+            self.request.GET.get('language')
+
+        if language:
+            response.set_cookie('_LOCALE_', value=language, max_age=31536000)
+
+        return HTTPFound(location='/', headers=response.headers)
+
+    def get_available_languages(self):
+        available_languages = sorted(literal_eval(
+            (self.settings.get('available_languages', '[]'))),
+            key=lambda tup: tup[1].lower())
+        return [
+            (code, self.get_display_name(code))
+            for code, name in available_languages]
+
+    def get_featured_languages(self):
+        featured_languages = sorted(literal_eval(
+            (self.settings.get('featured_languages', '[]'))),
+            key=lambda tup: tup[1].lower())
+        return [
+            (code, self.get_display_name(code))
+            for code, name in featured_languages]
+
+    def get_display_name(self, locale):
+        language_code, _, country_code = locale.partition('_')
+        term_code = languages.get(bibliographic=language_code).terminology
+        return Locale.parse(term_code).language_name
+
+    def get_display_languages(self):
+        print("--------------------------------------------------")
+        print(self.language)
+        print("--------------------------------------------------")
+        to_display = [
+            code for code, name in
+            self.get_featured_languages() or self.get_available_languages()[:2]
+        ]
+
+        featured_and_current = [self.language] + sorted(list(
+            set(to_display) - set([self.language])),
+            key=lambda tup: tup[1].lower())
+        return [
+            (code, self.get_display_name(code))
+            for code in featured_and_current]
