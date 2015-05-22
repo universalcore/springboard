3128e7590f73b37c5bc900809948204259c8d314
Simon de Haan <simon@praekeltfoundation.org>
2015-05-22 10:47:47 +0200

    util functions for parsing lists & dicts

--- springboard/tests/test_utils.py
+++ springboard/tests/test_utils.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from springboard.utils import parse_repo_name
+from springboard.utils import parse_repo_name, config_list, config_dict
 
 
 class TestUtils(TestCase):
@@ -18,3 +18,21 @@ class TestUtils(TestCase):
         self.assertParsed('foo', 'ssh://git@domain:repo/foo')
         self.assertParsed('foo', 'http://domain/repo/foo')
         self.assertParsed('foo', 'https://domain/repo/foo')
+
+    def test_config_list(self):
+        self.assertEqual(['1', '2', '3'], config_list('1\n2\n3'))
+        self.assertEqual(['1', '2', '3'], config_list('1\n2\n3\n'))
+        self.assertEqual([], config_list(''))
+
+    def tst_config_dict(self):
+        self.assertEqual({
+            'a': '1',
+            'b': '2',
+            'c': '3',
+        }, 'a=1\nb=2\nc=3')
+        self.assertEqual({
+            'a': '1',
+            'b': '2',
+            'c': '3',
+        }, 'a=1\nb=2\nc=3\n')
+        self.assertEqual({}, '')
