4e26ea803be8c959f5bab1d01d9b343e7223d616
Rizmari Versfeld <rizziepit@gmail.com>
2015-05-29 15:46:06 +0200

    make localisations available in templates

--- springboard/views/base.py
+++ springboard/views/base.py
@@ -4,7 +4,7 @@ from elasticgit.search import SM
 
 from springboard.utils import parse_repo_name, config_list
 
-from unicore.content.models import Category, Page
+from unicore.content.models import Category, Page, Localisation
 
 from slugify import slugify
 
@@ -38,6 +38,9 @@ class SpringboardViews(object):
         self.all_categories = SM(Category, **search_config).es(
             **self.es_settings)
         self.all_pages = SM(Page, **search_config).es(**self.es_settings)
+        self.all_localisations = SM(Localisation, **search_config).es(
+            **self.es_settings)
+
         self.available_languages = config_list(
             self.settings.get('available_languages', ''))
         self.featured_languages = config_list(
@@ -52,6 +55,7 @@ class SpringboardViews(object):
             'featured_languages': self.featured_languages,
             'display_languages': self.display_languages,
             'language': self.language,
+            'all_localisations': self.all_localisations,
             'all_categories': self.all_categories,
             'all_pages': self.all_pages,
         }
