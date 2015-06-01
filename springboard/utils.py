d548ebff1d78c05c4abc8f1f68cd531c2b391c69
Rizmari Versfeld <rizziepit@gmail.com>
2015-06-01 11:04:05 +0200

    port Paginator classes from unicore-cms

--- springboard/utils.py
+++ springboard/utils.py
@@ -1,6 +1,7 @@
 import re
 from functools import wraps
 from urlparse import urlparse
+import math
 
 
 def parse_repo_name(repo_url):
@@ -73,3 +74,110 @@ def config_dict(data):
     """
     lines = config_list(data)
     return dict(re.split('\s*=\s*', value) for value in lines)
+
+
+class Paginator(object):
+    """
+    A thing that helps us page through result sets
+
+    """
+
+    def __init__(self, results, page, results_per_page=10, slider_value=5):
+        self.results = results
+        self.page = page
+        self.results_per_page = results_per_page
+        self.slider_value = slider_value
+        self.buffer_value = self.slider_value / 2
+
+    def total_count(self):
+        return len(self.results)
+
+    def get_page(self):
+        return self.results[self.page * self.results_per_page:
+                            (self.page + 1) * self.results_per_page]
+
+    def has_next_page(self):
+        return ((self.page + 1) * self.results_per_page) < self.total_count()
+
+    def has_previous_page(self):
+        return self.page
+
+    def total_pages(self):
+        return int(
+            math.ceil(
+                float(self.total_count()) / float(self.results_per_page)))
+
+    def page_numbers(self):
+        if (self.page - self.buffer_value) < 0:
+            return [page_number
+                    for page_number in range(
+                        0, min([self.slider_value, self.total_pages()]))]
+        elif (self.page + self.buffer_value) >= self.total_pages():
+            return [page_number
+                    for page_number in range(
+                        max((self.total_pages() - self.slider_value), 0),
+                        self.total_pages())
+                    ]
+        else:
+            return range(self.page - self.buffer_value,
+                         self.page + self.buffer_value + 1)
+
+    def page_numbers_left(self):
+        page_numbers = self.page_numbers()
+        if not any(page_numbers):
+            return False
+        return page_numbers[:page_numbers.index(self.page)]
+
+    def page_numbers_right(self):
+        page_numbers = self.page_numbers()
+        if not any(page_numbers):
+            return False
+        return page_numbers[page_numbers.index(self.page) + 1:]
+
+    def needs_start_ellipsis(self):
+        page_numbers = self.page_numbers()
+        if not any(page_numbers):
+            return False
+        return page_numbers[0] > 1
+
+    def needs_end_ellipsis(self):
+        page_numbers = self.page_numbers()
+        if not any(page_numbers):
+            return False
+        return page_numbers[-1] < (self.total_pages() - 2)
+
+    def show_start(self):
+        page_numbers = self.page_numbers()
+        if not any(page_numbers):
+            return False
+        return page_numbers[0] > 0
+
+    def show_end(self):
+        page_numbers = self.page_numbers()
+        if not any(page_numbers):
+            return False
+        return page_numbers[-1] < self.total_pages() - 1
+
+
+class EGPaginator(Paginator):
+
+    class generator(object):
+        def __init__(self, es_results):
+            self.es_results = es_results
+
+        def __iter__(self):
+            return (obj.to_object() for obj in self.es_results)
+
+        def __len__(self):
+            return self.es_results.__len__()
+
+        def __getitem__(self, k):
+            if isinstance(k, slice):
+                return self.__class__(self.es_results.__getitem__(k))
+            return self.es_results.__getitem__(k).to_object()
+
+    def total_count(self):
+        return self.results.count()
+
+    def get_page(self):
+        return self.__class__.generator(super(EGPaginator, self).get_page())
