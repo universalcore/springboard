3b42277970c330e3f828a8de4c2fb0536bbb8ee4
nathanbegbie <nathanbegbie@gmail.com>
2015-06-08 12:21:58 +0200

    Added test for searching in selected language

--- springboard/tests/test_search.py
+++ springboard/tests/test_search.py
@@ -9,10 +9,21 @@ class TestSearch(SpringboardTestCase):
 
     def setUp(self):
         self.workspace = self.mk_workspace()
-        self.config = testing.setUp(settings={
+        settings = {
             'unicore.repos_dir': self.working_dir,
-            'unicore.content_repo_url': self.workspace.working_dir,
-        })
+            'unicore.content_repo_urls': self.workspace.working_dir,
+            'available_languages': '\n'.join([
+                'eng_GB',
+                'swa_KE',
+                'spa_ES',
+            ]),
+            'featured_languages': '\n'.join([
+                'spa_ES',
+                'eng_GB',
+            ])
+        }
+        self.config = testing.setUp(settings=settings)
+        self.app = self.mk_app(self.workspace, settings=settings)
 
     def tearDown(self):
         testing.tearDown()
@@ -76,3 +87,29 @@ class TestSearch(SpringboardTestCase):
             '/search/', params={'q': 'baby', 'p': '0'}, status=200)
         self.assertFalse('Previous' in resp.body)
         self.assertTrue('Next' in resp.body)
+
+    def test_search_language_filter(self):
+        [category_eng] = self.mk_categories(
+            self.workspace, count=1, language='eng_GB',
+            title='English Category')
+        self.mk_pages(
+            self.workspace, count=1, language='eng_GB',
+            primary_category=category_eng.uuid,
+            content='Page for mother test page')
+        [category_spa] = self.mk_categories(
+            self.workspace, count=1, language='spa_ES',
+            title='Spanish Category')
+        self.mk_pages(
+            self.workspace, count=1, language='spa_ES',
+            primary_category=category_spa.uuid,
+            content='Page for mother test page')
+
+        self.app.get('/locale/?language=eng_GB', status=302)
+        resp = self.app.get('/search/', params={'q': 'mother'}, status=200)
+        self.assertTrue('English Category' in resp.body)
+        self.assertFalse('Spanish Category' in resp.body)
+
+        self.app.get('/locale/?language=spa_ES', status=302)
+        resp = self.app.get('/search/', params={'q': 'mother'}, status=200)
+        self.assertTrue('Spanish Category' in resp.body)
+        self.assertFalse('English Category' in resp.body)
