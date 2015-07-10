87e5037b89132f652342ddf0e726dcf40e7f174e
Milton Madanda <madandat@gmail.com>
2015-07-10 10:36:03 +0200

    ensure celery is configured

--- springboard/config.py
+++ springboard/config.py
@@ -1,5 +1,6 @@
 def includeme(config):
     config.include('springboard.auth')
+    config.include("pyramid_celery")
     config.add_static_view('static', 'springboard:static', cache_max_age=3600)
     config.add_route('home', '/')
     config.add_route('category', '/category/{uuid}/')
