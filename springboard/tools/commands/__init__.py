92263fe6eb67b807aea4f1fd79063a3d3b541d8b
Rizmari Versfeld <rizziepit@gmail.com>
2015-07-20 15:42:33 +0200

    add update-messages command that can read languages from config

--- springboard/tools/commands/__init__.py
+++ springboard/tools/commands/__init__.py
@@ -5,6 +5,7 @@ from springboard.tools.commands.sync import SyncDataTool
 from springboard.tools.commands.bootstrap import BootstrapTool
 from springboard.tools.commands.startapp import StartAppTool
 from springboard.tools.commands.import_content import ImportContentTool
+from springboard.tools.commands.update_messages import UpdateMessagesTool
 
 __all__ = [
     'CloneRepoTool',
